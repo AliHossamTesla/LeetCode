7630903f41824b043aa1e4007acab15682e83f54
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2024-12-01 07:21:03 +0200

    Time: 612 ms (92.10%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,11 +1,2 @@
-SELECT 
-    s.user_id, 
-    COALESCE(ROUND(COUNT(CASE WHEN c.action = 'confirmed' THEN 1 END) / COUNT(c.action), 2), 0.00) AS confirmation_rate
-FROM 
-    Signups s
-LEFT JOIN 
-    Confirmations c
-ON 
-    s.user_id = c.user_id
-GROUP BY 
-    s.user_id;
+select s.user_id, round(avg(if(c.action="confirmed",1,0)),2) as confirmation_rate
+from Signups as s left join Confirmations as c on s.user_id= c.user_id group by user_id;
