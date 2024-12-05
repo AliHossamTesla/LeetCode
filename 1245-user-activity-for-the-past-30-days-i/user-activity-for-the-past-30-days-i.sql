f6d596a576ed9495210bd0133e41a658de936633
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2024-12-05 07:11:47 +0200

    Time: 766 ms (10.10%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -4,6 +4,6 @@ SELECT
     COUNT(DISTINCT user_id) AS active_users 
 FROM 
     Activity 
-WHERE 
-    activity_date >= '2019-06-28' AND activity_date <= '2019-07-27'
-GROUP BY activity_date;
+GROUP BY activity_date
+having  
+    activity_date >= '2019-06-28' AND activity_date <= '2019-07-27';
