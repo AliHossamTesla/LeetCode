4d15330eba01ce74254b85c02b90af7e30588d3e
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2024-12-05 07:10:17 +0200

    Time: 528 ms (58.76%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -5,5 +5,5 @@ SELECT
 FROM 
     Activity 
 WHERE 
-    activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
+    activity_date >= '2019-06-28' AND activity_date <= '2019-07-27'
 GROUP BY activity_date;
