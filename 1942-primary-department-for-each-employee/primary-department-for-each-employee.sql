c297d34e586b1c93995fdadea30ae30a90c5f572
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2024-12-08 07:08:18 +0200

    Time: 519 ms (88.09%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,12 +1,3 @@
-WITH SingleDept AS (
-    SELECT 
-        employee_id
-    FROM
-        Employee
-    GROUP BY 
-        employee_id
-    HAVING COUNT(department_id) = 1
-)
 SELECT 
     employee_id,
     department_id
@@ -16,13 +7,10 @@ WHERE
     primary_flag = 'Y'
 UNION ALL
 SELECT 
-    e.employee_id,
-    e.department_id
+    employee_id,
+    department_id
 FROM 
-    Employee e
-INNER JOIN 
-    SingleDept sd
-ON 
-    e.employee_id = sd.employee_id
-ORDER BY 
-    employee_id;
+    Employee
+GROUP BY
+    employee_id
+HAVING COUNT(department_id) = 1
