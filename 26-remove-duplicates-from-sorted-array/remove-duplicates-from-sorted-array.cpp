082fc5ad7bec5eff3a3a9c0fe4da69c3a45d8fc0
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2024-12-13 15:39:52 +0200

    Time: 0 ms (100.00%) | Memory: 22.7 MB (15.27%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& a) {
-        int slow = 0, fast = 0 ;
-        vector<bool>vis(201, false) ;
+        int slow = 1, fast = 1 ;
+        // vector<bool>vis(201, false) ;
         for(; fast < a.size() ; fast ++){
-            if(!vis[a[fast] + 100]){
-                vis[a[fast] + 100] = true ;
-                swap(a[fast], a[slow ++]) ;
+            if(a[fast] != a[fast - 1]){
+                a[slow ++] = a[fast] ;
             }
         }
         return slow ;
