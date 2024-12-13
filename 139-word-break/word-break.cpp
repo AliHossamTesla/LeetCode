c15faa5986005db8ce40447e9f9dac69cbdf99c6
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2024-12-13 17:52:53 +0200

    Time: 4 ms (63.12%) | Memory: 13 MB (58.65%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.size() ;
-        vector<bool>dp(n + 1, false) ;
-        dp[0] = 1;
-        unordered_set<string>st ;
-        for(auto c : wordDict)
-            st.insert(c) ;
+    struct Node{
+        Node* chld[26]{} ;
+        bool is_word ;
+        Node():is_word(false){
+            fill(begin(chld), end(chld), nullptr);
+        }
+    };
+    void insert_word(Node *root, string s){
+        Node* current_node = root ;
+        for(auto c : s){
+            if(current_node->chld[c - 'a'] == nullptr){
+                current_node->chld[c - 'a'] = new Node() ;
+            }
+            current_node = current_node->chld[c - 'a'] ;
+        }
+        current_node->is_word = true ;
         
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        Node *root = new Node() ;
+        for(auto c : wordDict){
+            insert_word(root, c) ;
+        }
+        
+        int n = s.size() ;
+        vector<bool>dp(n + 1);
+        dp[0] = true ;
         for(int i = 1 ; i <= n ; i ++){
-            for(int j = i ; j > 0 ; j --){
-                if(dp[j - 1] && st.count(s.substr(j - 1, i - j + 1))){
-                    dp[i] = true ;
-                    break ;
+            if(dp[i - 1]){
+                Node *current_node = root ;
+                for(int j = i - 1 ; j < n ; j ++){
+                    char c = s[j] ;
+                    if(current_node->chld[c - 'a'] == nullptr){
+                        break ;
+                    }
+                    current_node = current_node->chld[c - 'a'] ;
+                    if(current_node->is_word){
+                        dp[j + 1] = true ;
+                    }
                 }
             }
         }
