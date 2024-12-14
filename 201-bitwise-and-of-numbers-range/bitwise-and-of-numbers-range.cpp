82d809653f840fd219bbd8ddeb4cc43bd80779d0
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2024-12-14 09:21:22 +0200

    Time: 0 ms (100.00%) | Memory: 11.3 MB (14.81%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
-    int msbPosition(int n) {
-        return (n > 0) ? 31 - __builtin_clz(n) : -1; // For 32-bit integers
-    }
-
     int rangeBitwiseAnd(int l, int r) {
-        int ans = 0 ;
-        while(l && r){
-            int x = msbPosition(l), y = msbPosition(r) ;
-            if(x != y){
-                break ;
-            }
-            ans += (1 << x) ;
-            
-            l -= (1 << x) ;
-            r -= (1 << y) ;
+        while (l < r) {
+            r = r & (r - 1); // Remove the least significant bit of r
         }
-        return ans ;
+        return r;
     }
 };
