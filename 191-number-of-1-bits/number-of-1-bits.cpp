38c64c95dd8b483174cc7c3a255455e8bf8d2d83
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2024-12-14 09:00:32 +0200

    Time: 0 ms (100.00%) | Memory: 8.2 MB (13.68%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int ans = 0 ;
+        while(n){
+            ans += n % 2 ;
+            n/=2;
+        }
+        return ans ;
+    }
+};
