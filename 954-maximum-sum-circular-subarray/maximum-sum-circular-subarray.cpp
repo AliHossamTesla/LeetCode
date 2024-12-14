842843533f85bcfc6e776399c5c2e41752c68261
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2024-12-14 18:11:41 +0200

    Time: 0 ms (100.00%) | Memory: 43.5 MB (42.17%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& a) {
-        int sum = accumulate(begin(a), end(a), 0) ;
         int pref = 0, ans1 = INT_MAX, mx = 0, mn = 0, ans2 = INT_MIN;
         for(int i : a){
             pref += i ;
             ans1 = min(ans1, pref - mx) ;
             mx = max(mx, pref) ;
-            ans2 = max(ans2, pref - mn) ;;
+            ans2 = max(ans2, pref - mn) ;
             mn = min(mn, pref) ;
         }
-        if(ans1 == sum) return ans2 ;
-        return max(sum - ans1, ans2) ;
+        if(ans1 == pref) return ans2 ;
+        return max(pref - ans1, ans2) ;
     }
 };
