fb259bfa5bf3cc6e51d4b01f6c951e81a2020118
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2024-12-16 16:43:54 +0200

    Time: 3 ms (90.59%) | Memory: 30.9 MB (18.04%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& mat) {
-        int n = mat.size(), m = mat[0].size() ;
-        vector<vector<int>>pref(n + 1, vector<int>(m + 1, 0)) ;
-        for(int i = 1 ; i <= n ; i ++)
-            for(int j = 1 ; j <= m ; j ++)
-                pref[i][j] = pref[i][j - 1] + (mat[i - 1][j - 1] == '1') ;
+        int n = mat.size(), m = mat[0].size(), ans = 0;
+        vector<vector<int>>dp(n, vector<int>(m, 0)) ;
+        for(int i = 0 ; i < n ; i ++) 
+            dp[i][0] = (mat[i][0] == '1'), ans = max(ans, dp[i][0]);
+        for(int i = 0 ; i < m ; i ++)
+            dp[0][i] = (mat[0][i] == '1'), ans = max(ans, dp[0][i]);
         
-        for(int i = 1 ;i <= n ; i ++)
-            for(int j = 1 ; j <= m; j ++)
-                pref[i][j] += pref[i - 1][j] ;
-        
-        auto calc = [&](int i, int j, int k){
-            return pref[i + k + 1][j + k + 1] - 
-                   pref[i + k + 1][j] - pref[i][j + k + 1] + 
-                   pref[i][j] ;
-        };
-        int ans = 0 ;
-        for(int i = 0 ; i < n ; i ++){
-            for(int j = 0 ; j < m ; j ++){
-                for(int k = 0 ; k + i < n && k + j < m ; k ++){
-                    int cr = calc(i, j, k) ;
-                    // cout << i << " " << j << " " << k << " : " << cr << '\n' ;
-                    if(cr == (k + 1) * (k + 1)){
-                        ans = max(ans, (k + 1) * (k + 1)) ;
-                    }else break ;
-                }
+        for(int i = 1 ; i < n ; i ++){
+            for(int j = 1 ; j < m ; j ++)if(mat[i][j] == '1'){
+                dp[i][j] = 1 + min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) ;
+                ans = max(ans, dp[i][j]) ;
             }
         }
-        return ans ;
+        
+        return ans * ans ;
     }
 };
