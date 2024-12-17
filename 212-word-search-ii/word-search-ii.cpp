28ce296f7e32dbdc823a54a583c10adeed2bca3b
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2024-12-17 19:59:55 +0200

    Time: 2759 ms (5.03%) | Memory: 21.9 MB (18.76%) - LeetSync

---
+++ 212-word-search-ii/word-search-ii.cpp
@@ -0,0 +1,61 @@
+class Solution {
+private:
+    struct Node{
+        Node *ch[26]{} ;
+        bool is_word;
+        Node():is_word(false){
+            fill(ch,ch+26, nullptr) ;
+        }
+    };
+    Node *root = new Node() ;
+    void insert(string s){
+        Node *cr = root ;
+        for(auto c : s){
+            if(cr->ch[c - 'a'] == nullptr){
+                cr->ch[c - 'a'] = new Node() ;
+            }
+            cr = cr->ch[c - 'a'] ;
+        }
+        cr->is_word = true ;
+    }
+public:
+    vector<string> findWords(vector<vector<char>>& a, vector<string>& words) {
+        for(auto s : words){
+            insert(s) ;
+        }
+         int dx[]{1,0,-1,0}, dy[]{0,-1,0,1} ;
+        int n = a.size(), m =  a[0].size() ;
+        vector<string>ans ;
+        vector<vector<bool>>vis(n, vector<bool>(m, false)) ;
+        
+        auto valid = [&](int i, int j){
+            return i >= 0 && i < n && j >= 0 && j < m ;
+        };
+        
+        unordered_map<string, bool>mp ;
+        function<void(int, int, Node *, string)> floodfill = [&](int i, int j, Node *r, string s){
+            s.push_back(a[i][j]) ;
+            r = r->ch[a[i][j] - 'a'] ;
+            if(r == nullptr || vis[i][j]) return ;
+            if(r->is_word && mp.count(s) == 0){
+                ans.push_back(s) ;
+                mp[s] = true ;
+            }
+            
+            vis[i][j] = true ;
+            for(int di = 0 ; di < 4 ; di ++) if(valid(i + dx[di], j + dy[di])){
+                int ni = i + dx[di], nj = j + dy[di] ;
+                floodfill(ni, nj, r, s) ;
+            }
+            vis[i][j] = false ;
+        };
+        
+        for(int i = 0 ; i < n ; i ++){
+            for(int j = 0 ; j < m ; j ++){
+                vis = vector<vector<bool>>(n, vector<bool>(m, false)) ;
+                floodfill(i, j, root, "") ;
+            }
+        }
+        return ans ;
+    }
+};
