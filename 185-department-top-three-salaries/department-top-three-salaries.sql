77156288a00d86f1f5b203547387b841547ad2ee
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2024-12-30 16:55:03 +0200

    Time: 1247 ms (46.19%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,14 +1,20 @@
-WITH employee_department AS
-    (
-    SELECT d.id, 
-        d.name AS Department, 
-        salary AS Salary, 
-        e.name AS Employee, 
-        DENSE_RANK()OVER(PARTITION BY d.id ORDER BY salary DESC) AS rnk
-    FROM Department d
-    JOIN Employee e
-    ON d.id = e.departmentId
-    )
-SELECT Department, Employee, Salary
-FROM employee_department
-WHERE rnk <= 3
+WITH RankedSalaries AS (
+    SELECT 
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        DENSE_RANK() OVER (PARTITION BY d.id ORDER BY e.salary DESC) AS rnk
+    FROM 
+        Department d 
+    JOIN 
+        Employee e
+        ON e.departmentId = d.id
+)
+SELECT 
+    Department,
+    Employee,
+    Salary
+FROM 
+    RankedSalaries
+WHERE 
+    rnk <= 3;
