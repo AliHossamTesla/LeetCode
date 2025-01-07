a2309c4edf927f37092c0ef78280da69b3192419
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-01-07 06:21:53 +0200

    Time: 0 ms (100.00%) | Memory: 17.7 MB (19.52%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -1,19 +1,5 @@
 class Solution:
     def stringMatching(self, w: List[str]) -> List[str]:
-        ans = [] 
-
-        def is_sub(s, t):
-            if len(s) > len(t):
-                return False
-            for i in range(len(t) - len(s) + 1):
-                if s == t[i:i + len(s)]:
-                    return True
-            return False
-
-        for i in range(len(w)):
-            for j in range(len(w)):
-                if i != j and is_sub(w[i], w[j]):
-                    ans.append(w[i])
-                    break
-
+        t = ' '.join(w)
+        ans = [i for i in w if t.count(i) > 1]
         return ans 
