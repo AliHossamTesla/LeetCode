32f92f2ca83d95fc494105341470572f4b6a0c35
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-01-09 06:48:02 +0200

    Time: 0 ms (100.00%) | Memory: 13.8 MB (21.18%) - LeetSync

---
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int ans = 0 ;
+        for(auto s : words){
+            if(s.size() >= pref.size() && s.substr(0, pref.size()) == pref)
+                ans ++ ;
+        }
+        return ans ;
+    }
+};
