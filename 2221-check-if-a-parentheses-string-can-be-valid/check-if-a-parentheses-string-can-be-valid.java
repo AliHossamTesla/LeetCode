65ac798f05605c149ad21c5ce3d391b273337950
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-01-12 20:06:37 +0200

    Time: 19 ms (23.24%) | Memory: 45.8 MB (37.76%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
@@ -4,52 +4,28 @@ class Solution {
         if (n % 2 == 1) return false; // Odd-length strings cannot be balanced
 
         // Check from left to right (treat '(' as open)
-        if (!validate(s.toCharArray(), locked.toCharArray(), '(')) {
+        if (!validate(s, locked, '(')) {
             return false;
         }
 
         // Check from right to left (treat ')' as open)
-        if (!validateReverse(s.toCharArray(), locked.toCharArray(), ')')) {
+        if (!validate(new StringBuilder(s).reverse().toString(), 
+                      new StringBuilder(locked).reverse().toString(), ')')) {
             return false;
         }
 
         return true;
     }
 
-    private boolean validate(char[] s, char[] locked, char openChar) {
+    private boolean validate(String s, String locked, char openChar) {
         int balance = 0;
         int unlocked = 0;
 
-        for (int i = 0; i < s.length; i++) {
-            if (locked[i] == '0') {
+        for (int i = 0; i < s.length(); i++) {
+            if (locked.charAt(i) == '0') {
                 unlocked++; // Unlocked, can be used as either '(' or ')'
             } else {
-                if (s[i] == openChar) {
-                    balance++; // Increment balance for openChar
-                } else {
-                    balance--; // Decrement balance for closeChar
-                }
-            }
-
-            // If balance + unlocked < 0, it means we have more closeChars than openChars
-            // and we cannot fix it with unlocked characters
-            if (balance + unlocked < 0) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private boolean validateReverse(char[] s, char[] locked, char openChar) {
-        int balance = 0;
-        int unlocked = 0;
-
-        for (int i = s.length - 1; i >= 0; i--) {
-            if (locked[i] == '0') {
-                unlocked++; // Unlocked, can be used as either '(' or ')'
-            } else {
-                if (s[i] == openChar) {
+                if (s.charAt(i) == openChar) {
                     balance++; // Increment balance for openChar
                 } else {
                     balance--; // Decrement balance for closeChar
