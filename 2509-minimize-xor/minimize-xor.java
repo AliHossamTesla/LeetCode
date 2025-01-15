a22beee583bc6257cdd035047d2a4c40a8f1130d
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-01-15 06:17:53 +0200

    Time: 0 ms (100.00%) | Memory: 41 MB (21.70%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimizeXor(int num1, int num2) {
+        int cnt = 0 ;
+        for(int i = 0 ; i < 31 ; i ++)
+            cnt += (num2 >> i & 1) ;
+        
+        int ans = 0 ;
+        for(int i = 30 ; i >= 0 && cnt != 0 ; i --){
+            if((num1 >> i & 1) == 1){
+                ans |= (1 << i) ;
+                cnt -- ;
+            }
+        }
+        for(int i = 0 ;i < 31 && cnt != 0; i ++){
+            if((ans >> i & 1) == 0){
+                ans |= (1 << i) ;
+                cnt -- ;
+            }
+        }
+        return ans ;
+    }
+}
