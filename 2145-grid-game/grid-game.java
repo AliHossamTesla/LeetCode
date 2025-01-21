fa4395245bc80b4b82dcbe8183859660b0bd6fc5
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-01-21 06:51:49 +0200

    Time: 3 ms (100.00%) | Memory: 60.7 MB (5.18%) - LeetSync

--- 2145-grid-game/grid-game.java
+++ 2145-grid-game/grid-game.java
@@ -1,20 +1,15 @@
 class Solution {
     public long gridGame(int[][] g) {
-        int n = g[0].length;
-        if(n == 1) return 0 ;
-        long[] pref = new long[n];
-        long[] suf = new long[n] ;
-        pref[0] = g[1][0] ;
-        suf[n - 1] = g[0][n - 1] ;
-        for(int i = 1 ; i < n ; i ++){
-            pref[i] += pref[i - 1] + g[1][i];
-            suf[n - i - 1] += suf[n - i] + g[0][n - i - 1];
+        long ans = Long.MAX_VALUE ;
+        long pref = 0 ;
+        long suf = 0 ;
+        for(int i : g[0])
+            suf += i ;
+        for(int i = 0 ; i < g[0].length ; i ++){
+            suf -= g[0][i];
+            ans = Math.min(ans, Math.max(suf, pref)) ;
+            pref += g[1][i] ;
         }
-        long ans = suf[1] ;
-        for(int i = 1 ; i < n - 1 ; i ++){
-            ans = Math.min(ans, Math.max(suf[i + 1], pref[i - 1])) ;
-        }
-        ans = Math.min(ans, pref[n - 2]) ;
         return ans ;
     }
 }
