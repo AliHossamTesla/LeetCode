6b38a4c0dbbbc26cc56503706782a77e713cddf5
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-01-28 02:58:02 +0200

    Time: 5 ms (65.83%) | Memory: 45.4 MB (21.29%) - LeetSync

---
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
@@ -0,0 +1,26 @@
+class Solution {
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, 1, -1};
+    public int findMaxFish(int[][] g) {
+        int n = g.length;
+        int m = g[0].length;
+        boolean[][] vis = new boolean[n][m] ;
+        int ans = 0;
+        for(int i = 0 ; i < n ; i ++){
+            for(int j = 0 ; j < m ; j ++){
+                ans = Math.max(ans, floodfill(i, j, g, vis)) ;
+            }
+        }
+        return ans ;
+    }
+    private int floodfill(int x, int y, int[][] a, boolean[][] vis){
+        if(x < 0 || x >= a.length || y < 0 || y >= a[0].length || a[x][y] == 0 || vis[x][y])
+            return 0 ;
+        int ans = a[x][y] ;
+        vis[x][y] = true ;
+        for(int i = 0 ; i < 4 ; i ++){
+            ans += floodfill(x + dx[i], y + dy[i], a, vis);
+        }
+        return ans ;
+    }
+}
