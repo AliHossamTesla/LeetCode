a7abcb49b3669ef7ed9078c43ca8d2d277c00769
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-02-08 06:20:26 +0200

    Time: 74 ms (68.49%) | Memory: 99.3 MB (48.63%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.java
+++ 2434-design-a-number-container-system/design-a-number-container-system.java
@@ -1,29 +1,25 @@
+import java.util.*;
+
 class NumberContainers {
-    Map<Integer, Integer>idx;
-    Map<Integer, Set<Integer>> nums;
-    public NumberContainers() {
-        idx = new HashMap<>();
-        nums = new HashMap<>();
-    }
-    
+    private Map<Integer, Integer> idx = new HashMap<>();
+    private Map<Integer, TreeSet<Integer>> nums = new HashMap<>();
+
     public void change(int index, int number) {
-        nums.putIfAbsent(number, new TreeSet<>());
-        if(idx.containsKey(index)){
-            nums.get(idx.get(index)).remove(index);
+        int prevNumber = idx.getOrDefault(index, -1);
+        
+        // Remove index from the previous number's set if the number changes
+        if (prevNumber != -1 && prevNumber != number) {
+            nums.get(prevNumber).remove(index);
         }
+        
         idx.put(index, number);
-        nums.get(number).add(index);
+        
+        // Add index to the new number's set
+        nums.computeIfAbsent(number, k -> new TreeSet<>()).add(index);
     }
-    
+
     public int find(int number) {
-        if(!nums.containsKey(number) || nums.get(number).isEmpty()) return -1;
-        return nums.get(number).iterator().next();
+        TreeSet<Integer> indices = nums.get(number);
+        return (indices == null || indices.isEmpty()) ? -1 : indices.first();
     }
 }
-
-/**
- * Your NumberContainers object will be instantiated and called as such:
- * NumberContainers obj = new NumberContainers();
- * obj.change(index,number);
- * int param_2 = obj.find(number);
- */
