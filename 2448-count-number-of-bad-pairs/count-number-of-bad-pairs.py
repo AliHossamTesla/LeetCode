c389a86a5a8148f3dfc0a6d88f4f817196e5d13f
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-02-10 00:51:02 +0200

    Time: 67 ms (82.70%) | Memory: 39.4 MB (8.29%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -3,22 +3,11 @@ from typing import List
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
         n = len(nums)
-        a = [nums[i] - i for i in range(n)]
-        freq = {}
-        
-        # Count the frequency of each element in `a`
-        for i in a:
-            freq[i] = freq.get(i, 0) + 1
-        
-        # Calculate the total number of pairs
+        a = [v - i for i, v in enumerate(nums)]
+        cnt = Counter(a)
+
         total_pairs = n * (n - 1) // 2
+        for v in cnt.values():
+            total_pairs -= v * (v - 1) // 2
         
-        # Calculate the number of good pairs
-        good_pairs = 0
-        for v in freq.values():
-            good_pairs += v * (v - 1) // 2
-        
-        # Bad pairs = Total pairs - Good pairs
-        bad_pairs = total_pairs - good_pairs
-        
-        return bad_pairs
+        return total_pairs
