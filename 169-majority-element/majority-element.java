be626760910778161663120f82d5d57d8670c057
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-02-28 17:23:48 +0200

    Time: 4 ms (51.74%) | Memory: 53.8 MB (8.46%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,15 +1,6 @@
 class Solution {
     public int majorityElement(int[] a) {
-        int n = a.length;
-        int b = -1 ;
-        int cnt = 0 ;
-        for(int i = 0 ; i < n ; i ++){
-            if(cnt == 0){
-                b = a[i];
-            }
-            if(b == a[i]) cnt ++ ;
-            else cnt -- ;
-        }
-        return b ;
+        Arrays.sort(a);
+        return a[a.length/2];
     }
 }
