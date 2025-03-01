8e36a33ab4cbe41cc447246d30be4c58b638de55
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-03-01 03:45:55 +0200

    Time: 2 ms (71.04%) | Memory: 42.3 MB (7.25%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -2,8 +2,9 @@ class Solution {
     public int hIndex(int[] c) {
         int[] cnt = new int[1001];
         for(int i : c) cnt[i] ++ ;
-        for(int i = 999 ; i >= 0 ; i --) cnt[i] += cnt[i + 1];
-        for(int i = 1000 ; i >= 0 ; i --){
+        if(cnt[1000] >= 1000) return 1000 ;
+        for(int i = 999 ; i >= 0 ; i --){
+            cnt[i] += cnt[i + 1];
             if(cnt[i] >= i){
                 return i ;
             }
