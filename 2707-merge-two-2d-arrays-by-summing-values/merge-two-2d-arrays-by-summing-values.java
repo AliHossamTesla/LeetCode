b3243fc7e988932f4d4258932d3300bf1d561546
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-03-02 04:32:01 +0200

    Time: 1 ms (86.98%) | Memory: 45.6 MB (17.81%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -5,27 +5,18 @@ class Solution {
         int i1 = 0 ;
         int i2 = 0 ;
         List<int[]> ans = new ArrayList<>();
-        while(i1 < n || i2 < m){
-            if(i1 < n && i2 < m){
-                if(nums1[i1][0] == nums2[i2][0]){
-                    ans.add(new int[]{nums1[i1][0], nums1[i1][1] + nums2[i2][1]});
-                    i1 ++ ;
-                    i2 ++ ;
-                }else if(nums1[i1][0] < nums2[i2][0]){
-                    ans.add(new int[]{nums1[i1][0], nums1[i1][1]});
-                    i1 ++ ;
-                }else{
-                    ans.add(new int[]{nums2[i2][0], nums2[i2][1]});
-                    i2 ++ ;
-                }
-            }else if(i1 < n){
-                ans.add(new int[]{nums1[i1][0], nums1[i1][1]});
+        while(i1 < n && i2 < m){
+            if(nums1[i1][0] == nums2[i2][0]){
+                ans.add(new int[]{nums1[i1][0], nums1[i1][1] + nums2[i2][1]});
                 i1 ++ ;
-            }else{
-                ans.add(new int[]{nums2[i2][0], nums2[i2][1]});
                 i2 ++ ;
-            }
+            }else if(nums1[i1][0] < nums2[i2][0]) ans.add(nums1[i1++]);
+            else ans.add(nums2[i2++]);
         }
+        while(i1 < n)
+            ans.add(nums1[i1++]);
+        while(i2 < m)
+            ans.add(nums2[i2++]);
         int[][] ret = new int[ans.size()][2];
         for(int i = 0 ; i < ans.size() ; i ++){
             ret[i] = ans.get(i);
