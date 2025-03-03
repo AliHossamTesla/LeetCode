c8071eb059c59780641a1f278836e543a7115560
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-03-03 02:07:40 +0200

    Time: 8 ms (81.77%) | Memory: 58.1 MB (99.01%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.java
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,22 +1,16 @@
 class Solution {
-    int[] dp ;
-    int n ;
-    public boolean validPartition(int[] nums) {
-        n = nums.length;
-        dp = new int[n];
-        Arrays.fill(dp, -1);
-        return calc(0, nums) == 0 ? false : true ;
-    }
-    private int calc(int i, int[] a){
-        if(i == n) return 1 ;
-        if(dp[i] != -1) return dp[i] ;
-        dp[i] = 0 ;
-        if(i < n - 1 && a[i] == a[i + 1] && calc(i + 2, a) == 1)
-            dp[i] = 1 ;
-        if(i < n - 2 && a[i] == a[i + 1] && a[i] == a[i + 2] && calc(i + 3, a) == 1)
-            dp[i] = 1 ;
-        if(i < n - 2 && a[i] == a[i + 1] - 1 && a[i] == a[i + 2] - 2 && calc(i + 3, a) == 1)
-            dp[i] = 1 ;
-        return dp[i] ;
+    public boolean validPartition(int[] a) {
+        int n = a.length;
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for(int i = 0 ; i < n - 1; i ++){
+            if(a[i] == a[i + 1] && dp[i]) dp[i + 2] = true ;
+            if(i < n - 2){
+                if(a[i] == a[i + 1] && a[i] == a[i + 2] && dp[i]) dp[i + 3] = true ;
+                if(a[i] == a[i + 1] - 1 && a[i] == a[i + 2] - 2 && dp[i])
+                    dp[i + 3] = true ;
+            }
+        }
+        return dp[n];
     }
 }
