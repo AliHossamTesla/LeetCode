d8c5eee0647ad131665675c59b52bc04853e001f
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-03-03 04:29:02 +0200

    Time: 0 ms (100.00%) | Memory: 45.2 MB (95.62%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,12 +1,10 @@
 class Solution {
     public int singleNumber(int[] a) {
-        int n = a.length;
-        Arrays.sort(a);
-        for(int i = 1; i < n ; i += 3){
-            if(a[i] != a[i - 1]){
-                return a[i - 1];
-            }
+        int ones = 0, twos = 0 ;
+        for(int i : a){
+            ones ^= (i & ~twos);
+            twos ^= (i & ~ones);
         }
-        return a[n - 1];
+        return ones ;
     }
 }
