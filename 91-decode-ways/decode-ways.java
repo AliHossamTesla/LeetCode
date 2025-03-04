ce53d11a4f68684b1c18b68a4a482141aab1ddc0
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-03-04 06:25:16 +0200

    Time: 0 ms (100.00%) | Memory: 41.8 MB (76.02%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -3,13 +3,16 @@ class Solution {
     public int numDecodings(String s) {
         char[] a = s.toCharArray();
         int n = s.length() ;
-        dp = new int[n + 1] ;
-        dp[0] = 1 ;
+        int prv = 1 ;
+        int prv2 = 0 ;
         for(int i = 1 ; i <= n ; i ++){
-            if(a[i - 1] != '0') dp[i] += dp[i - 1];
+            int cr = 0 ;
+            if(a[i - 1] != '0') cr = prv;
             if(i > 1 && (a[i - 2] == '1' || (a[i - 2] == '2' && a[i - 1] <= '6')))
-                 dp[i] += dp[i - 2];
+                cr += prv2;
+            prv2 = prv ;
+            prv = cr ;
         }
-        return dp[n];
+        return prv;
     }
 }
