644d401ee9acf8dd2cd4958c5ba619967bef8950
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-03-04 07:05:40 +0200

    Time: 0 ms (100.00%) | Memory: 40.3 MB (82.38%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.java
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.java
@@ -1,14 +1,13 @@
 class Solution {
     public int numTilings(int n) {
         if(n <= 2) return n ;
-        long[] dp = new long[n + 1];
+        int[] dp = new int[n + 1];
         dp[1] = 1 ;
         dp[2] = 2 ;
         dp[3] = 5 ;
         for(int i = 4 ; i <= n ; i ++){
-            dp[i] = 2*dp[i - 1] + dp[i - 3];
-            dp[i] %= 1_000_000_007 ;
+            dp[i] = ((2 * dp[i - 1]) % 1_000_000_007 + dp[i - 3]) % 1_000_000_007 ;
         }
-        return (int)dp[n];
+        return dp[n];
     }
 }
