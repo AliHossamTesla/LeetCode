8def5b7f99e01bcc720d872a61d2f54a3dedd9d8
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-03-05 04:24:12 +0200

    Time: 0 ms (100.00%) | Memory: 40.5 MB (46.72%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -1,9 +1,5 @@
 class Solution {
     public long coloredCells(int n) {
-        long ans = 1 ;
-        for(int i = 2, j = 4; i <= n ; i ++, j += 4){
-            ans += j ;
-        }
-        return ans;
+        return 1L + 2L * (n - 1) * (n); 
     }
 }
