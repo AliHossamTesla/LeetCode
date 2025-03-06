cfe0f148c7adffe2d43a4220350780afc149a6c2
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-03-06 03:37:12 +0200

    Time: 2 ms (97.42%) | Memory: 44.2 MB (92.01%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -14,33 +14,14 @@
  * }
  */
 class Solution {
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        List<TreeNode> ans = trav(root, subRoot);
-        if(ans.size() == 0) return false ;
-        for(TreeNode t : ans){
-            if(dfs(t, subRoot)) return true ;
-        }
-        return false ;
+    public boolean isSubtree(TreeNode a, TreeNode b) {
+        if(a == null || b == null) return false ;
+        if(isSameTree(a, b)) return true ;
+        return isSubtree(a.left, b) || isSubtree(a.right, b);
     }
-    private boolean dfs(TreeNode a, TreeNode b){
+    public boolean isSameTree(TreeNode a, TreeNode b){
         if(a == null && b == null) return true ;
         if(a == null || b == null || a.val != b.val) return false;
-        if(!dfs(a.left, b.left)) return false ;
-        if(!dfs(a.right, b.right)) return false ;
-        return true ;
-    }
-    private List<TreeNode> trav(TreeNode root, TreeNode h){
-        ArrayDeque<TreeNode> t = new ArrayDeque<>();
-        t.add(root);
-        List<TreeNode> ans = new ArrayList<>();
-        while(!t.isEmpty()){
-            TreeNode cr = t.poll();
-            if(cr.val == h.val){
-                ans.add(cr);
-            }
-            if(cr.left != null) t.add(cr.left);
-            if(cr.right != null) t.add(cr.right);
-        }
-        return ans ;
+        return isSameTree(a.left, b.left) && isSameTree(a.right, b.right);
     }
 }
