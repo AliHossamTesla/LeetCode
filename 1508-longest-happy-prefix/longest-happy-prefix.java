20f64f31b4b3b3c675998d90995a168b59d5704c
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-03-06 04:47:08 +0200

    Time: 7 ms (93.75%) | Memory: 45.3 MB (93.32%) - LeetSync

---
+++ 1508-longest-happy-prefix/longest-happy-prefix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String longestPrefix(String s) {
+        int n = s.length() ;
+        char[] a = s.toCharArray();
+        int[] pi = new int[n];
+        for(int i = 1 ; i < n ; i ++){
+            int j = pi[i - 1];
+            while(j > 0 && a[i] != a[j]) j = pi[j - 1];
+            if(a[i] == a[j]) j ++ ;
+            pi[i] = j ;
+        }
+        return s.substring(0, pi[n - 1]);
+    }
+}
