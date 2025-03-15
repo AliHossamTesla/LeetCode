757c844c069c5761b550932c8177580bd8ec2451
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-03-15 04:17:59 +0200

    Time: 16 ms (99.50%) | Memory: 60 MB (28.11%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.java
+++ 2690-house-robber-iv/house-robber-iv.java
@@ -1,26 +1,23 @@
 class Solution {
-    public int minCapability(int[] nums, int k) {
+    public int minCapability(int[] a, int k) {
         int l = Integer.MAX_VALUE, r = Integer.MIN_VALUE;
-        for(int i : nums){
+        for(int i : a){
             l = Math.min(l, i);
             r = Math.max(r, i);
         }
         while(l <= r){
             int md = l + (r - l)/2 ;
-            if(ok(nums, md, k)){
+            int cnt = 0 ;
+            for(int i = 0 ; i < a.length ; i ++){
+                if(a[i] <= md){
+                    cnt ++ ;
+                    i ++ ;
+                }
+            }
+            if(cnt >= k){
                 r = md - 1;
             }else l = md + 1 ;
         }
         return l ;
-    }
-    private boolean ok(int[] a, int x, int k){
-        int cnt = 0 ;
-        for(int i = 0 ; i < a.length ; i ++){
-            if(a[i] <= x){
-                cnt ++ ;
-                i ++ ;
-            }
-        }
-        return cnt >= k ;
     }
 }
