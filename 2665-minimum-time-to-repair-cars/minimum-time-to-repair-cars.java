ceb0f059fd01ccb2d47d998e725fc7e507e39c1c
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-03-16 05:14:25 +0200

    Time: 65 ms (31.72%) | Memory: 56.7 MB (15.59%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long repairCars(int[] ranks, int cars) {
+        long mn = Arrays.stream(ranks).max().getAsInt();
+        long l = 0, r = cars * mn * cars ;
+        while(l <= r){
+            long md = l + (r - l)/2 ;
+            long cnt = 0 ;
+            for(int i : ranks){
+                cnt += Math.sqrt(md/i);
+            } 
+            if(cnt >= cars)
+                r = md - 1;
+            else l = md + 1;
+        }
+        return l ;
+    }
+}
