effe7570cc2ee1c269e348518b0e72e71aec9f0c
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-03-18 04:29:09 +0200

    Time: 11 ms (15.26%) | Memory: 55.9 MB (45.50%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.java
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -15,12 +15,6 @@ class Solution {
     }
 
     private int ct(int n){
-        int cnt = 0;
-        for(int i = 0 ; i < 31 ; i ++){
-            if(((n >> i) & 1) == 1){
-                cnt ++ ;
-            }
-        }
-        return cnt ;
+        return Integer.bitCount(n); 
     }
 }
