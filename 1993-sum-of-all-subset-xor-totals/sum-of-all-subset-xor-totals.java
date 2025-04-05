0280dbc9678c207df9e57a66caa71143b58be0c6
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-04-05 17:57:23 +0200

    Time: 5 ms (37.87%) | Memory: 41.5 MB (20.75%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,8 +1,15 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        int Or = 0 ;
-        for(int i : nums)
-            Or |= i ;
-        return (Or << (nums.length - 1));
+        int ans = 0 ;
+        for(int i = 1 ; i < (1 << nums.length) ; i ++){
+            int cr = 0 ;
+            for(int j = 0 ; j < nums.length ; j ++){
+                if((i >> j & 1) == 1){
+                    cr ^= nums[j];
+                }
+            }
+            ans += cr ;
+        }
+        return ans ;
     }
 }
