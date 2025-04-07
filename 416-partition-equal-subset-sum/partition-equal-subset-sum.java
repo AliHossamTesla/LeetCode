4e0efdc765d85344dc004cb511e5cdc8ccfb9823
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-04-07 02:24:39 +0200

    Time: 33 ms (68.56%) | Memory: 45.3 MB (71.63%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,21 +1,19 @@
 class Solution {
-    int[][] dp ;
-    private int calc(int[] a, int i, int sm){
-        if(sm == 0) return 1 ;
-        if(i == a.length) return 0 ;
-        if(sm < 0) return 0 ;
-        if(dp[i][sm] != -1) return dp[i][sm] ;
-        if(calc(a, i + 1, sm - a[i]) == 1) dp[i][sm] = 1 ;
-        else if(calc(a, i + 1, sm) == 1) dp[i][sm] = 1;
-        else dp[i][sm] = 0 ;
-        return dp[i][sm] ;
-    }
-    public boolean canPartition(int[] nums) {
+    public boolean canPartition(int[] a) {
         int sum = 0 ;
-        for(int i : nums) sum += i ;
+        for(int i : a) sum += i ;
         if(sum % 2 != 0) return false ;
-        dp = new int[nums.length][sum/2 + 1] ;
-        for(int i = 0;i < nums.length; i ++) Arrays.fill(dp[i], -1) ;
-        return calc(nums, 0, sum/2) == 1;
+        int n = a.length;
+        sum /= 2;
+        boolean[] dp = new boolean[sum + 1] ;
+        dp[0] = true ;
+        for(int i = 0 ; i < n ; i ++){
+            boolean[] ndp = dp.clone() ;
+            for(int s = a[i] ; s <= sum ; s ++){
+                ndp[s] |= dp[s - a[i]] ;
+            }
+            dp = ndp ;
+        }
+        return dp[sum] ;
     }
 }
