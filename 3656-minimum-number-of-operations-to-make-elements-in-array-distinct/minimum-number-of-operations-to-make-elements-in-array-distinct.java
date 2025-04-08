8fadf729dce7f155696e97f067114cf9f91ac765
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-04-08 02:21:49 +0200

    Time: 1 ms (98.39%) | Memory: 44.2 MB (84.84%) - LeetSync

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int[] cnt = new int[101] ;
+        for(int i = nums.length - 1 ;i >= 0 ; i --){
+            if(++cnt[nums[i]] > 1){
+                return (i + 3)/3 ;
+            }
+        }
+        return 0 ;
+    }
+}
