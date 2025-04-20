2646c1f3ed90730991ef3a96243901e922a5036e
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-04-20 02:26:01 +0200

    Time: 55 ms (61.39%) | Memory: 65.7 MB (5.05%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -3,14 +3,13 @@ public:
     long long countFairPairs(vector<int>& nums, int lo, int hi) {
         vector<int> seen; // sorted vector
         long long ans = 0;
-
+        sort(begin(nums), end(nums)) ;
         for (int i : nums) {
             int l = lower_bound(seen.begin(), seen.end(), lo - i) - seen.begin();
             int r = upper_bound(seen.begin(), seen.end(), hi - i) - seen.begin();
             ans += r - l;
-            seen.insert(upper_bound(seen.begin(), seen.end(), i), i); // keep sorted
+            seen.push_back(i);
         }
-
         return ans;
     }
 };
