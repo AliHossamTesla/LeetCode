16fffa9a3a8f68b6d27e3a7488ca7bea8ca12d33
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-05-07 05:13:06 +0300

    Time: 39 ms (21.73%) | Memory: 30.7 MB (29.88%) - LeetSync

---
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& a) {
+        int n = a.size(), m = a[0].size() ;
+        vector<vector<int>>dist(n, vector<int>(m, INT_MAX)) ;
+        int dx[]{0, 0, -1, 1}, dy[]{1, -1, 0, 0} ;
+        priority_queue<array<int, 3>, vector<array<int, 3>>, greater<>>pq ;
+        pq.push({0, 0, 0}) ;
+        while(!pq.empty()){
+            auto [d, x, y] = pq.top() ; pq.pop() ;
+            if(dist[x][y] <= d) continue ;
+            if(x == n - 1 && y == m - 1) return d ;
+            dist[x][y] = d ;
+            for(int i = 0 ; i < 4 ; i ++){
+                int nx = x + dx[i], ny = y + dy[i] ;
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue ;
+                pq.push({max(d, a[nx][ny]) + 1, nx, ny}) ;
+            }
+        }
+        return -1 ;
+    }
+};
