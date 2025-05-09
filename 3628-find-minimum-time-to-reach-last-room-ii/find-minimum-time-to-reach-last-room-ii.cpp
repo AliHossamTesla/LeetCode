3e8a33631bc2c46aa905f5a3028f7137744acfa1
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-05-09 03:22:23 +0300

    Time: 1054 ms (23.69%) | Memory: 102.7 MB (68.95%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
@@ -4,17 +4,17 @@ public:
         int n = a.size(), m = a[0].size() ;
         vector<vector<int>>dist(n, vector<int>(m, INT_MAX)) ;
         int dx[]{0, 0, -1, 1}, dy[]{1, -1, 0, 0} ;
-        priority_queue<array<int, 4>, vector<array<int, 4>>, greater<>>pq ;
-        pq.push({0, 0, 0, 0}) ;
+        priority_queue<array<int, 3>, vector<array<int, 3>>, greater<>>pq ;
+        pq.push({0, 0, 0}) ;
         while(!pq.empty()){
-            auto [d, x, y, st] = pq.top() ; pq.pop() ;
+            auto [d, x, y] = pq.top() ; pq.pop() ;
             if(dist[x][y] <= d) continue ;
             if(x == n - 1 && y == m - 1) return d ;
-            dist[x][y] = d ;
+            dist[x][y] = d;
             for(int i = 0 ; i < 4 ; i ++){
                 int nx = x + dx[i], ny = y + dy[i] ;
                 if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue ;
-                pq.push({max(d, a[nx][ny]) + 1 + st, nx, ny, 1 - st}) ;
+                pq.push({max(d, a[nx][ny]) + 1 + (x + y)%2, nx, ny}) ;
             }
         }
         return -1 ;
