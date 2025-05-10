20a2a83a4e725ea6f3b1b4d07663a3ee086e1383
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-05-10 07:52:15 +0300

    Time: 146 ms (11.53%) | Memory: 135.5 MB (15.59%) - LeetSync

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     long long minSum(vector<int>& a, vector<int>& b) {
-        long long x = -1, y = -1, l = 0, r = 0 ;
+        long long l = 0, r = 0 ;
+        bool x = 0, y = 0 ;
         for(int i = 0 ; i < a.size() ; i ++){
-            if(a[i] == 0 && x == -1){
-                x = i ;
-            }
-            if(a[i] == 0) a[i] = 1;
-            l += a[i] ;
+            if(a[i] == 0){
+                l ++ ;
+                x = 1 ;
+            }else l += a[i] ;
         }
         for(int i = 0 ; i < b.size() ; i ++){
-            if(b[i] == 0 && y == -1){
-                y = i ;
-            }
-            if(b[i] == 0) b[i] = 1;
-            r += b[i] ;
+            if(b[i] == 0){
+                r ++ ;
+                y = 1;
+            }else r += b[i] ;
         }
         if(l == r){
             return l ;
-        }
-        if((l > r && y != -1) || (r > l && x != -1)){
+        }else if((l > r && y) || (r > l && x)){
             return max(l, r) ;
         }else return -1 ;
     }
