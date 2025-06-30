3bf9770f6399ea7466f91c2795f161fee7d50bbf
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-06-30 03:21:11 +0300

    Time: 58 ms (7.95%) | Memory: 45.7 MB (9.72%) - LeetSync

---
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findLHS(vector<int>& a) {
+        map<int, int>mp ;
+        for(int i : a) mp[i] ++ ;
+        int ans = 0 ;
+        for(auto it : mp)if(mp.contains(it.first - 1)){
+            ans = max(ans, it.second + mp[it.first - 1]) ;
+        }
+        return ans ;
+    }
+};
