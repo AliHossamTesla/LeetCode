bb6f2fe58984ab8cf2c1c0fb86744e9b7c3c457b
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-07-21 07:08:05 +0300

    Time: 58 ms (9.18%) | Memory: 44.7 MB (13.57%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string t ;
+        for(char c : s){
+            if(t.size() >= 2 && t.back() == c && t[t.size() - 2] == c)
+                t.pop_back();
+            t.push_back(c) ;
+        }
+        return t ;
+    }
+};
