720aebd7f0f0268b5ab164414bc46cf6cc218e57
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-08-16 19:41:33 +0300

    Time: 104 ms (23.73%) | Memory: 144.9 MB (25.17%) - LeetSync

---
+++ 3963-number-of-perfect-pairs/number-of-perfect-pairs.cpp
@@ -0,0 +1,15 @@
+#define i64 long long
+class Solution {
+public:
+    long long perfectPairs(vector<int>& nums) {
+        vector<int>a;
+        for(int i : nums) a.push_back(abs(i)) ;
+        sort(begin(a), end(a)) ;
+        i64 ans = 0 ;
+        for(int i = 0 ; i < a.size() ; i ++){
+            int j = upper_bound(begin(a), end(a), 2*a[i]) - begin(a) ;
+            ans += j - i - 1;
+        }
+        return ans ;
+    }
+};
